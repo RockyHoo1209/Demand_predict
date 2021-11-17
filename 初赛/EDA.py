9b2ffff2f58205c2804553ac2b3fa991817a8774
RockyHoo1209 <1284219022@qq.com>
2021-11-17 22:54:22 +0800

    EDA思路

---
+++ 初赛/EDA.py
@@ -0,0 +1,9 @@
+'''
+Author: RockyHoo
+Date: 2021-11-17 22:49:10
+LastEditTime: 2021-11-17 22:53:29
+Description: 数据探索,主要思路是运用散点图
+1.不同特征维度的变化趋势 2.同一特征不同时间步长的变化趋势 3.借助stats.tsa进行时间序列分析
+FilePath: \Demand_predict\初赛\EDA.py
+'''
+refer="https://www.kaggle.com/adityaecdrid/my-first-time-series-comp-added-prophet"
